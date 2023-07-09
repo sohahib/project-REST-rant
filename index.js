8013eb968a52d3d2ea2127ac83ac8a3473717abd
sohahib <syaoda@yahoo.fr>
2023-07-09 10:24:21 -0400

    Adding 404 page view

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.get('/', (req, res)=>{
     res.render('home')
 })
 
+app.get('*', (req, res) => {
+    res.render('error404')
+  })
+  
+  
 
-
-
-app.get('*', (req,res)=>{
-    res.status(404).send(`<h1>404 Page</h1>`)
-})
 app.listen(process.env.PORT)
