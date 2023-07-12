da3d266ddfff6bd0e92cd7801d33c1dfa92cd711
sohahib <syaoda@yahoo.fr>
2023-07-11 20:36:50 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,9 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const places = require('../models/places')
 
-
-    
-app.get('/', (req,res)=>{
-      let places = [{
-            name: 'H-Thai-ML',
-            city: 'Seattle',
-            state: 'WA',
-            cuisines: 'Thai, Pan-Asian',
-            pic: '/images/rant.jpg'
-          }, {
-            name: 'Coding Cat Cafe',
-            city: 'Phoenix',
-            state: 'AZ',
-            cuisines: 'Coffee, Bakery',
-            pic: '/images/cafe.jpg'
-          }]
-          
+app.get('/', (req,res)=>{          
       res.render('places/index', {places})
 })
 
@@ -28,7 +13,17 @@ app.get('/new', (req, res) => {
 
 app.post('/', (req, res) => {
       console.log(req.body)
-      res.send('POST /places')
+      if(!req.body.pic){
+            req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if(!req.body.city){
+            req.body.city = 'Anytown'
+      }
+      if(!req.body.state){
+            req.body.state = 'USA'
+      }
+      places.push(req.body)
+      res.redirect('/places')
     })
     
 
