c8fbcd5960f4b9c48c2232e9b6021638064f1d91
sohahib <syaoda@yahoo.fr>
2023-07-11 20:18:53 -0400

    Form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -26,7 +26,10 @@ app.get('/new', (req, res) => {
       res.render('places/new')
     })
 
-
+app.post('/', (req, res) => {
+      console.log(req.body)
+      res.send('POST /places')
+    })
     
 
 module.exports = app
