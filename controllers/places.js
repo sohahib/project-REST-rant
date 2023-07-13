62d77662566dac959392b38f394174b3b143b18c
sohahib <syaoda@yahoo.fr>
2023-07-13 06:03:08 -0400

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -18,7 +18,7 @@ app.get('/:id', (req, res) => {
         res.render('error404')
       }
       else {
-        res.render('places/show', {places: places[id]})
+        res.render('places/show', {places: places[id], id})
       }
     })
     
@@ -36,5 +36,19 @@ app.post('/', (req, res) => {
       places.push(req.body)
       res.redirect('/places')
     })
+app.delete('/:id', (req, res) => {
+      let id = Number(req.params.id)
+      if (isNaN(id)) {
+        res.render('error404')
+      }
+      else if (!places[id]) {
+        res.render('error404')
+      }
+      else {
+            places.splice(id, 1)
+            res.redirect('/places')
+      }
+    })
+     
 
 module.exports = app
