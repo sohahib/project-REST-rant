cb0eb87759ea0b18d05286ea0698da0141d221ce
sohahib <syaoda@yahoo.fr>
2023-07-24 11:16:13 -0400

    Setting up comments

--- models/index.js
+++ models/index.js
@@ -6,3 +6,4 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 })
 module.exports.Place = require('./places')
+module.exports.Comment = require('./comment')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+  let comments = (
+    <h3 className='inactive'>
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
         <Def>
           <main>
@@ -27,7 +46,7 @@ function show (data) {
             </div>
             <div className="comments">
               <h1>Comments</h1>
-              <p>No comments yet</p>
+              {comments}
             </div>
               <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                Edit
@@ -44,3 +63,6 @@ function show (data) {
 }  
 
 module.exports = show
+
+
+
